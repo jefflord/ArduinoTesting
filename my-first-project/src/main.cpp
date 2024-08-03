05e0e70d14e51b232cd90a4c566122f3f044118e
Jeff Lord <jeff@jefflord.com>
2024-08-03 13:20:35 -0400

    Faster transfer!

--- my-first-project/src/main.cpp
+++ my-first-project/src/main.cpp
@@ -6,15 +6,41 @@ int myFunction(int, int);
 int counter = 0;
 void setup()
 {
-  Serial.begin(9600);
+  //Serial.begin(9600);
+  Serial.begin(230400);
+  
   while (!Serial)
     continue;
   // put your setup code here, to run once:
 }
 
+String formatNumber(int num) {
+  String result = String(num);
+  int len = result.length();
+  int commaCount = (len - 1) / 3; // Calculate how many commas are needed
+  
+  if (commaCount > 0) { // Only proceed if commas are needed
+    String formattedResult = "";  // Create a new string to store the formatted result
+    int index = len - 1;  // Start at the end of the original string
+
+    for (int i = 0; i < len; i++) { 
+      formattedResult = result[index] + formattedResult; // Add characters in reverse
+      if ((i + 1) % 3 == 0 && i != len - 1) { 
+        formattedResult = "," + formattedResult; // Add a comma every 3 digits
+      }
+      index--;
+    }
+
+    return formattedResult; 
+  } else {
+    return result; // No commas needed, return the original string
+  }
+}
+
 void loop()
 {
   delay(1000);
+  counter += 10000000 * 4;
   Serial.print(counter++);
   Serial.println();
   // put your main code here, to run repeatedly:
@@ -23,7 +49,12 @@ void loop()
 
   // Add values in the document
   doc["sensor"] = "gps";
-  doc["time"] = 1351824120 + counter;
+
+  doc["counter"] = counter;
+  doc["buffer"] = formatNumber(counter);
+
+  Serial.print("buffer:");
+  Serial.println(formatNumber(counter));
 
   // Add an array
   JsonArray data = doc["data"].to<JsonArray>();
@@ -41,14 +72,14 @@ void loop()
   // Generate the prettified JSON and send it to the Serial port
   serializeJsonPretty(doc, Serial);
 
-  Serial.println("");
-  Serial.println("---");
-  Serial.println(ARDUINO);
-  Serial.println(__FILE__);
-  Serial.println(__DATE__);
-  Serial.println(__TIME__);
-  Serial.println(__LINE__);
-  Serial.println("---");
+  // Serial.println("");
+  // Serial.println("---");
+  // Serial.println(ARDUINO);
+  // Serial.println(__FILE__);
+  // Serial.println(__DATE__);
+  // Serial.println(__TIME__);
+  // Serial.println(__LINE__);
+  // Serial.println("---");
 }
 
 // put function definitions here:
